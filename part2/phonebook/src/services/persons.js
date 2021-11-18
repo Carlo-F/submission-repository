88ffda2dc136c47beb1039978812142f69135cba
Carlo Feniello <carlofeniello@hotmail.it>
2021-11-18 17:52:01 +0100

    Changed baseUrl after build

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'https://ancient-caverns-47626.herokuapp.com/api/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
