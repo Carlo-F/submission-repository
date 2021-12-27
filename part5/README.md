ca9c109fa4fd7d5063c2f49f35a8b5388a58f383
Carlo Feniello <carlofeniello@hotmail.it>
2021-12-27 17:02:38 +0100

    Exercise 5.1 completed

---
+++ part5/README.md
@@ -0,0 +1,4 @@
+# Exercises of part 5
+
+
+- [x] 5.1 bloglist frontend step 1
---
+++ part5/src/services/login.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+const baseUrl = '/api/login'
+
+let token = null
+
+const setToken = async () => {
+
+}
+
+const login = async credentials => {
+    const response = await axios.post(baseUrl, credentials)
+    return response.data
+}
+
+export default {login}
