a803f97d6d8ed6d8c3738dfdc3d7a488cee169af
Carlo Feniello <carlofeniello@hotmail.it>
2021-12-30 16:03:34 +0100

    Exercise 5.7 completed

--- part5/README.md
+++ part5/README.md
@@ -7,7 +7,7 @@
 - [x] 5.4 bloglist frontend step 4
 - [x] 5.5 bloglist frontend step 5
 - [x] 5.6 bloglist frontend step 6
-- [ ] 5.7 bloglist frontend step 7
+- [x] 5.7 bloglist frontend step 7
 - [ ] 5.8 bloglist frontend step 8
 - [ ] 5.9 bloglist frontend step 9
 - [ ] 5.10 bloglist frontend step 10
--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -1,8 +1,24 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    <strong>{blog.title}</strong> by <strong>{blog.author}</strong>
-  </div>  
-)
+import React, {useState} from 'react'
+const Blog = ({ blog }) => {
+  
+  const [blogDetailsVisible, setBlogDetailsVisible] = useState(false)
+  const hideWhenVisible = { display: blogDetailsVisible ? 'none' : '' }
+  const showWhenVisible = { display: blogDetailsVisible ? '' : 'none' }
+      
+  return (
+    <div>
+      <p>
+        <strong>{blog.title}</strong>
+        <button onClick={() => setBlogDetailsVisible(!blogDetailsVisible)}>{blogDetailsVisible ? 'hide' : 'show details'}</button>
+      </p>
+      <ul style={showWhenVisible}>
+        <li>author: {blog.author}</li>
+        <li>url: {blog.url}</li>
+        <li>likes: {blog.likes} <button>like</button></li>
+      </ul>
+
+    </div>
+  )
+}
 
 export default Blog
